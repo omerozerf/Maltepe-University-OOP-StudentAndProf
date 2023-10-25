606051003cf9cf98d7a811e2e60d6cb46f8045b4
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-10-25 17:30:07 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ public class Main
     public static void main(String[] args)
     {
         Prof omerProf = new Prof(25, "SE");
-        Prof idilProf = new Prof(45, "ASD");
+        Prof cerenProf = new Prof(45, "ASD");
 
         Student student1 = new Student(456845463, "Numan");
-        Student student2 = new Student(684684, "Selen");
+        Student student2 = new Student(684684, "Ceren");
         Student qdıohuqhu = new Student(879685485, "daıjwq");
 
         omerProf.Teaches();
+        cerenProf.Teaches();
 
         student2.Learns();
         qdıohuqhu.Learns();
